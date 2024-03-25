07559378cbf2296781655571d01c3b0de6307c0b
PhanAnhTuan123 <108232105+PhanAnhTuan123@users.noreply.github.com>
2024-03-25 23:37:37 +0700

    init_fly

---
+++ springboot_for_flyway/src/main/java/dev/anhTuan/springboot_for_flyway/SpringbootForFlywayApplication.java
@@ -0,0 +1,13 @@
+package dev.anhTuan.springboot_for_flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootForFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootForFlywayApplication.class, args);
+	}
+
+}
---
+++ springboot_for_flyway/src/test/java/dev/anhTuan/springboot_for_flyway/SpringbootForFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anhTuan.springboot_for_flyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootForFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
