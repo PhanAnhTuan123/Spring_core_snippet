fe4bb5b777921648ee2d3cb34a73900c69a0ed12
PhanAnhTuan123 <108232105+PhanAnhTuan123@users.noreply.github.com>
2024-04-03 16:45:28 +0700

    pushed

---
+++ spring-jpa-crud/src/main/java/dev/anhTuan/demo/domain/Author.java
@@ -0,0 +1,9 @@
+package dev.anhTuan.demo.domain;
+
+public class Author {
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+}
---
+++ spring-jpa-crud/src/main/java/dev/anhTuan/demo/domain/Book.java
@@ -0,0 +1,4 @@
+package dev.anhTuan.demo.domain;
+
+public class Book {
+}
