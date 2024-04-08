6a1190d360199c0b45016cb85d07ade3f0a89e32
PhanAnhTuan123 <108232105+PhanAnhTuan123@users.noreply.github.com>
2024-04-08 08:57:56 +0700

    test_completed

--- rest_api/src/test/java/dev/anhTuan_rest_api/rest_api/repositories/AuthorEntityRepositoryIntergrationTest.java
+++ rest_api/src/test/java/dev/anhTuan_rest_api/rest_api/repositories/AuthorEntityRepositoryIntergrationTest.java
@@ -1,4 +1,34 @@
 package dev.anhTuan_rest_api.rest_api.repositories;
 
+import dev.anhTuan_rest_api.rest_api.TestDataUtil;
+import dev.anhTuan_rest_api.rest_api.domain.enties.AuthorEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AuthorEntityRepositoryIntergrationTest {
+    private AuthorRepository underTest;
+
+    @Autowired
+    public void setUnderTest(AuthorRepository underTest) {
+        this.underTest = underTest;
+    }
+
+    @Test
+    public void testThatAuthorCanBeCreatedAndRecalled(){
+        AuthorEntity authorEntity = TestDataUtil.createTestAuthorEntityA();
+        underTest.save(authorEntity);
+        Optional<AuthorEntity>result = underTest.findById(authorEntity.getId());
+        Assertions.assertThat(result).isPresent();
+        Assertions.assertThat(result.get()).isEqualTo(authorEntity);
+    }
 }
