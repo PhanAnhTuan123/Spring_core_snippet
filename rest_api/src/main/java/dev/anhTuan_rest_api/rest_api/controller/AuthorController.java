5f5eabac04449d30b861b7feb9fe5924a4870c96
PhanAnhTuan123 <108232105+PhanAnhTuan123@users.noreply.github.com>
2024-04-09 00:39:15 +0700

    push

--- rest_api/src/main/java/dev/anhTuan_rest_api/rest_api/controller/AuthorController.java
+++ rest_api/src/main/java/dev/anhTuan_rest_api/rest_api/controller/AuthorController.java
@@ -6,9 +6,13 @@ import dev.anhTuan_rest_api.rest_api.mappers.Mapper;
 import dev.anhTuan_rest_api.rest_api.services.AuthorServices;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AuthorController {
@@ -27,5 +31,10 @@ public class AuthorController {
         AuthorEntity savedAuthorEntity = authorServices.save(authorEntity);
 
         return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
+    }
+    @GetMapping(path = "/authors")
+    public List<AuthorDTO>listAuthors(){
+        List<AuthorEntity>authors =  authorServices.findAll();
+        return authors.stream().map(authorMapper::mapTo).collect(Collectors.toList());
     }
 }
--- rest_api/src/test/java/dev/anhTuan_rest_api/rest_api/repositories/AuthorEntityRepositoryIntergrationTest.java
+++ rest_api/src/test/java/dev/anhTuan_rest_api/rest_api/repositories/AuthorEntityRepositoryIntergrationTest.java
@@ -1,26 +1,38 @@
 package dev.anhTuan_rest_api.rest_api.repositories;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.anhTuan_rest_api.rest_api.TestDataUtil;
+import dev.anhTuan_rest_api.rest_api.domain.dto.AuthorDTO;
 import dev.anhTuan_rest_api.rest_api.domain.enties.AuthorEntity;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Optional;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
 public class AuthorEntityRepositoryIntergrationTest {
     private AuthorRepository underTest;
+    private ObjectMapper objectMapper;
 
+    private MockMvc mockMvc;
     @Autowired
-    public void setUnderTest(AuthorRepository underTest) {
+    public void setUnderTest(AuthorRepository underTest,ObjectMapper objectMapper,MockMvc mockMvc) {
         this.underTest = underTest;
+        this.mockMvc = mockMvc;
+        this.objectMapper = objectMapper;
     }
 
     @Test
@@ -81,6 +93,56 @@ public class AuthorEntityRepositoryIntergrationTest {
 
         Iterable<AuthorEntity>result = underTest.findAuthorsWithAgeGreaterThan(50);
         Assertions.assertThat(result).containsExactly(testAuthorAEntity);
+    }
+    @Test
+    public void testDeletedAuthor(){
+        AuthorEntity authorEntity = TestDataUtil.createTestAuthorEntityA();
+        underTest.save(authorEntity);
+        underTest.deleteById(authorEntity.getId());
+        Optional<AuthorEntity>result = underTest.findById(authorEntity.getId());
+        Assertions.assertThat(result).isEmpty();
+    }
+    @Test
+    public void testThatCreateAuthorSucceessfullyReturnSavedAuthor()throws  Exception{
+        AuthorDTO testAuthorDtoA = TestDataUtil.createTestAuthorDtoA();
+        testAuthorDtoA.setId(null);
+        String authorJson = objectMapper.writeValueAsString(testAuthorDtoA);
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/authors")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(authorJson)
+
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.id").isNumber()
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.name").value("Abigail Rose")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.age").value(80)
+        );
+
+    }
+    @Test
+    public void testThatListAuthorsReturnHttpStatus200() throws  Exception{
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/authors")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+    @Test
+    public void testThatListAuthorsReturnListOfAuthors() throws  Exception{
+        AuthorEntity testAuthorA  = TestDataUtil.createTestAuthorEntityA();
+        underTest.save(testAuthorA);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/authors")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$[0].id").isNumber()
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$[0].name").value("Abigail Rose")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$[0].age").value(80)
+        );
     }
 
 }
