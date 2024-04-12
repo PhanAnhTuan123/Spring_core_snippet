c3a617fc79a485a3a417fcbdf698c7ec98b36162
PhanAnhTuan123 <108232105+PhanAnhTuan123@users.noreply.github.com>
2024-04-12 23:34:01 +0700

    push

--- validationDemo/src/main/java/dev/anhTuan/validationDemo/mvc/Customer.java
+++ validationDemo/src/main/java/dev/anhTuan/validationDemo/mvc/Customer.java
@@ -1,5 +1,6 @@
 package dev.anhTuan.validationDemo.mvc;
 
+import dev.anhTuan.validationDemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
@@ -16,12 +17,15 @@ public class Customer {
 
 
     @NotNull(message = "is required")
-    @Min(value = 0,message = "must be greater than or equal to zero")
+    @Min(value = 0,message = "must be greater than or equal  to zero")
     @Max(value = 10,message = "must be greater than or equal to 10")
     private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}",message = "only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value = "TUAN123",message = "must start be TUAN123")
+    private String courseCode;
 
 
 }
---
+++ validationDemo/src/main/java/dev/anhTuan/validationDemo/mvc/validation/CorseCodeConstraintValidator.java
@@ -0,0 +1,28 @@
+package dev.anhTuan.validationDemo.mvc.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CorseCodeConstraintValidator implements ConstraintValidator<CourseCode,String> {
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode theCourseCode) {
+        coursePrefix = theCourseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
+
+        boolean result;
+
+        if (theCode != null) {
+            result = theCode.startsWith(coursePrefix);
+        }
+        else {
+            result = true;
+        }
+
+        return result;
+    }
+}
